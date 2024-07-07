82e13aea9df7e45855d24580c76a83c70ea8d8ea
Souvik Biswas <souvik.biswas1998@gmail.com>
2024-07-07 17:51:00 +0530

    create grocery mfe with initial code

--- projects/grocery/src/app/app.component.ts
+++ projects/grocery/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'host';
+  title = 'grocery';
 }
--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'grocery',
+        loadComponent: () =>
+            loadRemoteModule('grocery', './Component').then((m) => m.AppComponent),
+    },
+];
